fc70864393ec5365087d774cf792f8ec519d15da
Conor Quinn <cjq234@gmail.com>
2022-05-17 17:58:08 +0100

    separating camera control loop from user choice and further improvements

--- RPS-Cam-game.py
+++ RPS-Cam-game.py
@@ -16,28 +16,38 @@ botweapon = ['rock', 'paper', 'scissors']
 userweapon = ['nothing','rock', 'paper', 'scissors']
 user_score = 0
 computer_score = 0
-bot_choice = ''
-user_choice = ''
 
 # Function list
 
 # Bot weapon
-def get_computer_choice():
+def get_bot_choice():
     bot_choice = random.choice(botweapon)
     return bot_choice
 
 # Countdown
 def countdown():
-    print(f'Bot chose {bot_choice}! ')
-    print('Who will emerge victorious? ')
+    print('Who has emerged victorious? ')
     count = 3
     while count >= 1:
         print(count)
         time.sleep(1)
         count -= 1
 
-def who_won_round():
+def get_user_choice(prediction):
+    print('PREPARE TO FIGHT! DISPLAY YOUR WEAPON! ')
+    time.sleep(1)
+    # fetch weapon from prediction
+    index = argmax(prediction)
+    user_choice = userweapon[index]
+    time.sleep(3)    
+
+        
+    print(f'You have chosen the way of the {user_choice}! ')          
+    return user_choice
+
+def who_won_round(bot_choice, user_choice):
     #decision making
+    print(f'Bot chose {bot_choice}! ')
     if bot_choice == 'rock':
         if user_choice == 'paper':
             user_score += 1
@@ -82,42 +92,7 @@ def who_won_round():
     if user_choice == 'nothing ':
         computer_score += 1
         print('You failed to raise your weapon, you shameful coward! ')
-
-
-def get_user_choice():
-    user_choice = ''
-    print('PREPARE TO FIGHT! DISPLAY YOUR WEAPON! PRESS \'Q\' TO FIGHT ')
-    while True: 
-        ret, frame = cap.read()
-        resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
-        image_np = np.array(resized_frame)
-        normalized_image = (image_np.astype(np.float32) / 127.0) - 1
-        # Normalize the image
-        data[0] = normalized_image
-        prediction = model.predict(data)
-        cv2.imshow('frame', frame)
-
-        # fetch index of
-        index = argmax(prediction)
-
-        # print(prediction)
-        
-        if not user_choice == userweapon[index]:
-            user_choice = userweapon[index]
-            print(f'You wish to pick the {user_choice}?')
-
-        # Press q to close the window
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    # After the loop release the cap object
-    cap.release()
-    # Destroy all the windows
-    cv2.destroyAllWindows()
-    print(f'You have chosen the way of the {user_choice}! ')          
-    return user_choice
-
-
+    
 
 def resolve_winner():
     if user_score == 2:
@@ -126,21 +101,39 @@ def resolve_winner():
     if computer_score == 2:
         print("The Computer has won 2 rounds, they are declared winner!")
 
+def match_end():
+    print("ggwp no re")
+    # After the loop release the cap object
+    cap.release()
+    # Destroy all the windows
+    cv2.destroyAllWindows()
+
 # Resolution
-def play_a_round():
-    get_computer_choice()
-    get_user_choice()
+def play_a_round(prediction):
+    user1 = get_user_choice(prediction)
+    bot1 = get_bot_choice()
     countdown()
-    who_won_round()
+    who_won_round(bot1, user1)
     
-
 def play():
+    while True: 
+        ret, frame = cap.read()
+        resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
+        image_np = np.array(resized_frame)
+        normalized_image = (image_np.astype(np.float32) / 127.0) - 1
         
-    while user_score or computer_score <= 2:
-        play_a_round()
+        # Normalize the image
+        data[0] = normalized_image
+        prediction = model.predict(data)
+        cv2.imshow('frame', frame)
+        
+        # Press q to close the window
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break   
+        if not user_score or computer_score < 2:
+            play_a_round(prediction)
     
-    resolve_winner()
+        resolve_winner()
+        match_end()
     
-    return
-
 play()
